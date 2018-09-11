0473d3f0b02741b1019e45933315a56821bcde1d
Haines <3003534080@qq.com>
2018-09-11 18:21:54 +0800

    update

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,2 +1,49 @@
-const path = require('path')
-const webapck = require('webpack')
+const path           = require('path')
+const fs             = require('fs')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const srcDir         = path.resolve(process.cwd(), 'src')
+
+const getEntry = function () {
+	let source = path.resolve(srcDir, 'js')
+	let dirs = fs.readdirSync(source)
+	let matchs = [], files = {}
+
+	dirs.forEach(function (item) {
+		matchs = item.match(/(.+)\.js/)
+
+		if (matchs) {
+			files[matchs[1]] = path.resolve(srcDir, 'js', item)
+		}
+	})
+	return files
+}
+
+module.exports = {
+	// cache: false,
+	// devtool: '#',
+	mode: 'development',
+	entry: getEntry(),
+	output: {
+		path: path.join(__dirname, '../dist/js/'),
+		publicPath: 'dist/js/',
+		filename: '[name].js',
+		chunkFilename: '[chunkhash].js'
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all'
+		},
+		minimizer: [
+			new UglifyJsPlugin({
+				uglifyOptions: {
+					warnings: false,
+					compress: {
+						drop_debugger: false
+					}
+				}
+				
+			})
+		]
+	},
+	plugins: []
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,114 +1,133 @@
-/**
- *  gulp 常用插件
- *  https://github.com/lin-xin/blog/issues/2
- * */
+const gulp         = require('gulp')
+const gutil        = require('gulp-util')
+const changed      = require('gulp-changed')
+const sass         = require('gulp-sass')
+const base64       = require('gulp-base64')
+const autoprefixer = require('gulp-autoprefixer')
+const imagemin     = require('gulp-imagemin')
+const md5          = require('gulp-md5-plus')
+const del          = require('del')
+const browserSync  = require('browser-sync')
+const yargs        = require('yargs').argv
+const runSequence  = require('run-sequence')
+const pngquant     = require('imagemin-pngquant')
+const webpack      = require('webpack')
+const webpackConfig = require('./config/webpack.config')
 
-let gulp = require('gulp'),
-    browserSync = require('browser-sync'),
-    yargs = require('yargs').argv,
-    runSequence = require('run-sequence'),
-    pngquant = require('imagemin-pngquant'),
-    $ = require('gulp-load-plugins')()
 
 const dest = './dist',
-     outstyle = {
-        n:'nested',
-        e:'expanded',
-        pt:'compact',
-        pd:'compressed'
-     }
+	 outstyle = {
+		n:'nested',
+		e:'expanded',
+		pt:'compact',
+		pd:'compressed'
+	 }
 
 gulp.task('css', () => {
-    gulp.src('./src/scss/**/*', {base: './src/scss'})
-        .pipe($.changed(dest, {hasChanged: $.changed.compareSha1Digest}))
-        .pipe($.sass({outputStyle: outstyle.pd}))
-        .pipe($.autoprefixer())
-        .pipe($.base64())
-        .pipe(gulp.dest(dest + '/css'))
+	gulp.src('./src/scss/**/*', {base: './src/scss'})
+		.pipe(changed(dest, {hasChanged: changed.compareSha1Digest}))
+		.pipe(sass({outputStyle: outstyle.pd}))
+		.pipe(autoprefixer())
+		.pipe(base64())
+		.pipe(gulp.dest(`${dest}/css`))
 })
 
-gulp.task('js', () => {
-    gulp.src(['./src/js/**/*.js', './src/libs/**/*.js'], {base: './src/js'})
-        .pipe($.changed(dest, {hasChanged: $.changed.compareSha1Digest}))
-        .pipe(gulp.dest(dest + '/js'))
+gulp.task('js', (done) => {
+	webpack(webpackConfig, (err, stats) => {
+		if (err) {
+			throw new gutil.PluginError('webpack:', err)
+		} else {
+			gutil.log('[webpack]', stats.toString({
+				colors: true
+			}))
+		}
+		done()
+	})
+})
+
+gulp.task('md5', ['clean'], () => {
+	gulp.src([`${dest}/js/*.js`, `${dest}/css/*.css`], {base: dest})
+        .pipe(md5(10, `${dest}/*.html`))
+		.pipe(gulp.dest(`${dest}`))
 })
 
 gulp.task('img', () => {
-    gulp.src(['./src/img/**/*.jpg', './src/img/**/*.png', './src/img/**/*.gif', './src/img/**/*.svg'], {base: './src/img/'})
-    .pipe($.imagemin([
-        $.imagemin.gifsicle({interlaced: true}),
-        $.imagemin.jpegtran({progressive: true}),
-        $.imagemin.optipng({
-            optimizationLevel: 7
-            // ,
-            // plugins: [
-            //     pngquant({})
-            // ]
-        }),
-        $.imagemin.svgo({
-            plugins: [
-                {removeViewBox: false},
-                {cleanupIDs: false}
-            ]
-        })
-    ]))
-    .pipe(gulp.dest(dest + '/img'))
+	gulp.src(['./src/img/**/*.jpg', './src/img/**/*.png', './src/img/**/*.gif', './src/img/**/*.svg'], {base: './src/img/'})
+	.pipe(imagemin([
+		imagemin.gifsicle({interlaced: true}),
+		imagemin.jpegtran({progressive: true}),
+		imagemin.optipng({
+			optimizationLevel: 7
+			// ,
+			// plugins: [
+			//	 pngquant({})
+			// ]
+		}),
+		imagemin.svgo({
+			plugins: [
+				{removeViewBox: false},
+				{cleanupIDs: false}
+			]
+		})
+	]))
+	.pipe(gulp.dest(`${dest}/img`))
 })
 
 
 gulp.task('html', () => {
-    gulp.src(['./src/*.html', './src/temp/**/*'], {base: './src'})
-    .pipe($.changed(dest, {hasChanged: $.changed.compareSha1Digest}))
-    .pipe(gulp.dest(dest))
+	gulp.src(['./src/*.html', './src/temp/**/*'], {base: './src'})
+	.pipe(changed(dest, {hasChanged: changed.compareSha1Digest}))
+	.pipe(gulp.dest(dest))
 })
 
 gulp.task('clean', () => {
-    gulp.src(dest, {read:false})
-        .pipe($.clean())
+	del(dest)
 })
 
 gulp.task('watch', () => {
-    gulp.watch('src/*.html', ['html'])
-    gulp.watch('src/scss/**/*.scss', ['css'])
-    gulp.watch('src/js/**/*.js', ['js'])
-    gulp.watch(['src/img/**/*.jpg', './src/img/**/*.png', './src/img/**/*.gif', './src/img/**/*.svg'], ['img'])
-    
-    browserSync.init([
-        dest+'/css/*.css',
-        dest+'*.html',
-        dest+'/js/*.js',
-        dest+'/img/**/*'
-    ], {
-        server: {
-            baseDir: dest
-        }
-    })
+	gulp.watch('src/*.html', ['html'])
+	gulp.watch('src/scss/**/*.scss', ['css'])
+	gulp.watch('src/js/**/*.js', ['js'])
+	gulp.watch(['src/img/**/*.jpg', './src/img/**/*.png', './src/img/**/*.gif', './src/img/**/*.svg'], ['img'])
+	
+	browserSync.init([
+        `${dest}/css/*.css`,
+        `${dest}/*.html`,
+        `${dest}/js/*.js`,
+        `${dest}/img/**/*`
+	], {
+		server: {
+			baseDir: dest
+		}
+	})
 })
 
-gulp.task('build', cb => {
-    runSequence('clean', ['css', 'js', 'html', 'img'],cb)
+gulp.task('build:dev', cb => {
+	runSequence('clean', ['css', 'js', 'html', 'img'],cb)
+})
+
+gulp.task('build:live', cb => {
+	runSequence(['md5'], cb)
 })
 
 /**
- * gulp
- * gulp -c 
- * gulp -w
- * gulp -m
+ * run task
  */
 
 gulp.task('default', () =>{
-    switch (true) {
-        case yargs.w: 
-            gulp.start('watch')
-            break
-        case yargs.c: 
-            gulp.start('clean')
-            break
-        case yargs.m:
-            gulp.start('img')
-            break
-        default:
-            gulp.start('build')
-            gulp.start('watch')  
-    }
+	switch (true) {
+		case yargs.c: 
+			gulp.start('clean')
+			break
+		case yargs.d:
+            gulp.start('build:dev')
+			gulp.start('watch')
+			break
+		case yargs.l: 
+			gulp.start('build:live')
+			break
+		default:
+			gulp.start('build:dev')
+			gulp.start('watch')
+	}
 })
