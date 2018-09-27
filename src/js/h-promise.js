b0adbb1a2f403b0866381df17f14c91f66ec47ad
haines <haines@haines.local>
2018-09-28 01:43:49 +0800

    update native promise

--- src/js/h-promise.js
+++ src/js/h-promise.js
@@ -15,14 +15,6 @@ const timer = (typeof setImmediate !== 'undefined') ?
 				function timer(fn) {return setImmediate(fn)} :
 				setTimeout
 
-const defineProto = function(obj, name, val, config) {
-	return Object.defineProperty(obj, name, {
-		value: val,
-		writable: true,
-		configurable: config !== false
-	})
-}
-
 const queue = (function() {
 	var first, last, item
 
@@ -69,10 +61,10 @@ const schedule = function (fn, self) {
 	queue.add(fn, self)
 
 	/**
-	 * 将优先出列的函数放到执行线程的队尾
+	 * 将优先出列的函数放到执行队列的队尾
 	*/
 	if (!cycle) {
-		cycle.timer(queue.drain)
+		cycle = timer(queue.drain)
 	}
 }
 
@@ -87,3 +79,194 @@ const isThenable = function (o) {
 	return typeof then == 'function' ? then : false
 }
 
+const notify = function () {
+	for (let i = 0, l = this.chain.length; i < l; i++) {
+		notifyIsolated(this, (this.state === 1) ? this.chain[i].success: this.chain[i].failure, this.chain[i])
+	}
+}
+
+const notifyIsolated = function(self, cb, chain) {
+	let ret,_then
+	try {
+		if (cb === false) {
+			chain.reject(self.msg)
+		} else {
+			if (cb === true) {
+				ret = self.msg
+			} else {
+				ret = cb.call(void 0, self.msg)
+			}
+
+			if (ret === chain.promise) {
+				chain.reject(new TypeError('Promise chain cycle'))
+			} else if (_this = isThenable(ret)) {
+				_then.call(ret, chain.resolve, chain.reject)
+			} else {
+				chain.resolve(ret)
+			}
+		}
+
+	} catch(err) {
+		chain.reject(err)
+	}
+}
+
+const resolve = function (msg) {
+	let _then, self = this
+
+	if (self.triggered) {
+		return
+	}
+
+	self.triggered = true
+
+	if (self.def) {
+		self = self.def
+	}
+
+	try {
+		if (_then = isThenable(msg)) {
+			schedule(function() {
+				let def_wrapper = new MakeDefWrapper(self)
+				try {
+					_then.call(msg, () => {
+						resolve.apply(def_wrapper, arguments)
+					}, () => {
+						reject.apply(def_wrapper, arguments)
+					})
+
+				} catch(err) {
+					reject.call(def_wrapper, err)
+				}
+			}, self)
+		} else {
+			self.msg = msg
+			self.state = 1
+			if (self.chain.length > 0) {
+				schedule(chain, self)
+			}
+		}
+
+	} catch (err) {
+		reject.call(new MakeDefWrapper(self), err)
+	}
+}
+
+const reject = function (msg) {
+	let self = this
+
+	if (self.triggered) {
+		return
+	}
+
+	self.triggered = true
+
+	if (self.def) {
+		self = self.def
+	}
+
+	self.msg = msg
+	self.state = 2
+	if (self.chain.length > 0) {
+		schedule(notify, self)
+	}
+}
+
+
+function MakeDefWrapper (self) {
+	this.def = self
+	this.triggered = false
+}
+
+function MakeDef (self) {
+	this.promise = self
+	this.state = 0
+	this.triggered = false
+	this.chain = []
+	this.msg = void 0
+}
+
+function Promise (executor) {
+	if (typeof executor != 'function') {
+		throw TypeError('Not a function')
+	}
+
+	if (this.__NPO__ !== 0) {
+		throw TypeError('Not a promise')
+	}
+
+	this.__NPO__ = 1
+
+	let def = new MakeDef(this)
+
+	this.then = function (success, failure) {
+		let o = {
+			success: typeof success == 'function' ? success : false,
+			failure: typeof failure == 'failure' ? failure : false
+		}
+
+		o.promise = new this.constructor(function(resolve, reject) {
+			if (typeof resolve != 'function' || typeof reject != 'function') {
+				throw TypeError('Not a function')
+			}
+
+			o.resolve = resolve
+			o.reject = reject
+		})
+
+		def.chain.push(o)
+
+		if (def.state !== 0) {
+			schedule(notify, def)
+		}
+
+		return o.promise
+
+	}
+
+	this.catch = function (failure) {
+		return this.then(void 0, failure)
+	}
+
+	executor.call(void 0, function(msg) {
+		resolve.call(def, msg)
+	}, function(msg) {
+		reject.call(def, msg)
+	})
+}
+
+Promise.prototype.__NPO__ = 0
+
+Promise.resolve  = msg => {
+
+	if (msg && typeof msg == 'object' && msg.__NPO__ === 1) {
+		return msg
+	}
+
+	return new this((resolve, reject) => {
+		if (typeof resolve != 'function' || typeof reject != 'function') {
+			throw TypeError('Not a function')
+		}
+
+		resolve(msg)
+	})
+}
+
+Promise.reject = msg => {
+
+	return new this((resolve, reject) => {
+		if (typeof resolve != 'function' || typeof reject != 'function') {
+			throw TypeError('Not a function')
+		}
+
+		reject(msg)
+	})
+}
+
+Promise['all'] = function (msg) {
+
+}
+
+Promise.race = function (msg) {
+
+}
--- src/js/test3.js
+++ src/js/test3.js
@@ -1,6 +1,7 @@
 'use strict';
 
 console.log(1)
+
 new Promise(resolve => {
 	console.log(2)
 	resolve()
@@ -8,4 +9,41 @@ new Promise(resolve => {
 .then(res => {
 	console.log(3)
 })
+
+Promise.resolve(function() {
+	return 5
+})
+.then(res => {
+	console.log(res())
+})
+
+
 console.log(4)
+
+
+function T (fn) {
+	fn.call(void 0, arguments)
+}
+
+
+// T.foo = function (msg) {
+// 	return new T(function () {
+// 		console.log('msg', msg)
+// 	})
+// }
+
+Object.defineProperty(T, 'foo', {
+	value: msg => {
+		var self = T
+		/**
+		 * babel 会将这里的this 转化为_this
+		*/
+		return new self(function () {
+			console.log('msg:', msg)
+		})
+	},
+	writable: true,
+	configurable: false
+})
+
+T.foo('ss')
