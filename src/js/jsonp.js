e9140b644c6d56e0674d53c4fd9e74490aa69d6d
haines <haines@haines.local>
2018-10-16 01:00:09 +0800

    update

--- src/js/jsonp.js
+++ src/js/jsonp.js
@@ -1,20 +1,27 @@
 import 'whatwg-fetch'
 
-fetch('http://localhost:1688/api/v1/jsonp?callback=cbs', {
-	method: 'GET',
-	// headers: new Headers({
-	// 	'Access-Control-Allow-Origin': '*',
-	// 	'Content-Type': 'application/json'
-	// }),
-	// mode: 'no-cors',
-	// credentials: 'include',
-	// redirect: 'follow',
-	// integrity,
-	// cache: 'no-cache'
-})
-.then(res => {
-	return res.text()
-})
-.then(data => {
-	console.log('data', data)
-})
+// fetch('http://localhost:1688/api/v1/jsonp?callback=cbs', {
+// 	method: 'GET',
+// 	// headers: new Headers({
+// 	// 	'Access-Control-Allow-Origin': '*',
+// 	// 	'Content-Type': 'application/json'
+// 	// }),
+// 	// mode: 'no-cors',
+// 	// credentials: 'include',
+// 	// redirect: 'follow',
+// 	// integrity,
+// 	// cache: 'no-cache'
+// })
+// .then(res => {
+// 	return res.text()
+// })
+// .then(data => {
+// 	console.log('data', data)
+// })
+
+
+import { loadScript } from '../utils'
+
+loadScript('http://localhost:1688/api/v1/jsonp?callback=evalCallback')
+
+// loadScript('/js/test2.js')
--- src/utils/util.js
+++ src/utils/util.js
@@ -82,3 +82,32 @@ export function debounceImmediate (fn, ms, immediate) {
 	return wrap
 
 }
+
+/**
+ *
+ *
+ * @export
+ * @param {* String} url
+ * @param {* Function} callback
+ * @param {* Object} options
+ */
+export function loadScript (url, callback, ...options) {
+	let script = document.createElement('script')
+
+	script.type = 'text/javascript'
+	script.defer = options.defer || true
+
+	if (script.readyState) {	// for IE10-
+		script.onreadystatechange = () => {
+			if (script.readyState == 'loaded' || script.readyState == 'complete') {
+				script.onreadystatechange = null // GC
+				callback()
+			}
+		}
+	} else {
+		script.onload = callback
+	}
+
+	script.src = url
+	document.head.appendChild(script)
+}
