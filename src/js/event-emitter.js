9ccbede884d69741b8f483b37f375d4de0072773
haines <haines@haines.local>
2018-10-29 17:48:40 +0800

    event emit es6 class

--- src/js/event-emitter.js
+++ src/js/event-emitter.js
@@ -1,89 +1,90 @@
-(function () {
-    'use strict';
-
-    var Emitter = function () {
-        if (!this.hasOwnProperty('_events')) {
-            this._events = {}
-        }
-    }
-
-    Emitter.prototype = {
-        on (event, fn, scope) {
-            if (!fn) {
-                console.error('error handler')
-            }
-
-            var events = this._events[event] || (this._events[event] = [])
-
-            events.push({
-                fn: fn,
-                scope: scope || null
-            })
-
-            return this
-        },
-        off (event, fn) {
-            if (!fn) {
-                console.error('error handler')
-            }
-
-            var events = this._events[event]
-
-            if (!events) return this
-
-            if (arguments.length === 1) {
-                delete this._events[event]
-
-                return this
-            }
-
-            var cb
-            for (var i = 0, l = events.length; i < l; i++) {
-                cb = events[i]
-
-                if (cb === fn || cb.fn === fn) {
-                    events.splice(i, 1)
-                    break
-                }
-            }
-
-            return this
-        },
-        once(event, fn) {
-            var self = this
-
-            function on () {
-                self.off(event, on)
-                fn.apply(this, arguments)
-            }
-
-            this.on(event, on, this)
-            return this
-        },
-        emit(event) {
-           var args = [].slice.call(arguments, 1)
-           var events = this._events[event]
-
-           if (events) {
-               events = events.slice(0)
-
-               console.log('events', events)
-
-               for (var i = 0, len = events.length; i < len; ++i) {
-                    events[i].fn.apply(this, args)
-               }
-           }
-
-           return this
-        }
+class Emitter {
+	constructor () {
+		if (!this.hasOwnProperty('_events')) {
+			this._events = {}
+		}
 	}
 
-    var emmiter = new Emitter()
+	on (event, fn, ctx) {
+		if (!fn) {
+			console.error('Error handler')
+		}
 
-    function handler () {
-        console.log('arguments', arguments)
-    }
+		let events = this._events[event] || (this._events[event] = [])
 
-    emmiter.on('event', handler, this)
-	emmiter.emit('event', 'hihao')
-} ())
+		events.push({
+			fn: fn,
+			ctx: ctx
+		})
+
+		return this // 链式
+	}
+
+	off (event, fn) {
+		if (!fn) {
+			console.error('Error handler')
+		}
+
+		let events = this._events[event]
+
+		if (!events) {
+			return this
+		}
+
+		if (arguments.length === 1) {
+			delete this._events[event]
+
+			return this
+		}
+
+		for (let i = 0, cb = null, len = events.length; i < len; i++) {
+			cb = events[i]
+
+			if (cb === fn || cb.fn === fn) {
+				events.splice(i, 1)
+				break
+			}
+		}
+
+		return this
+	}
+
+	once (event, fn) {
+		let wrap, ctx = this
+
+		wrap = function () {
+			ctx.off(event, wrap)
+			fn.apply(this, arguments)
+		}
+
+		this.on(event, wrap, this)
+	}
+
+	/**
+	 * trigger an event
+	 * */
+	emit (event) {
+		let args = [].slice.call(arguments, 1)
+		let events = this._events[event]
+
+		if (events) {
+			events = events.slice(0)
+
+			for (let i = 0, len = events.length; i < len; i++) {
+				events[i].fn.apply(this, args)
+			}
+		}
+
+		return this
+	}
+}
+
+
+var emmiter = new Emitter()
+
+function handler (e) {
+	console.log('arguments', e)
+}
+
+emmiter.on('event', handler, this)
+emmiter.emit('event', 'hihao').emit('event', 'ceshi')
